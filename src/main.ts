fbb4d64de89b05c9d2bbd4c646d048a95a8e162c
PrenitaVulpo <guilhermepdl97@protonmail.com>
2022-11-28 23:05:25 -0300

    add colision

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
-import { jump } from "./components/Mario";
+import { jump, mario } from "./components/Mario";
 import { pipe } from "./components/Pipe";
 
 const gameLoop = setInterval(() => {
 	const pipePos = pipe.offsetLeft;
+	const marioPos = +window.getComputedStyle(mario).bottom.replace("px", "");
+
+	if (pipePos <= 120 && pipePos > 0 && marioPos < 80) {
+		pipe.style.animation = "none";
+		pipe.style.left = `${pipePos}px`;
+
+		mario.style.animation = "none";
+		mario.style.bottom = `${marioPos}px`;
+	}
 }, 10);
 
 document.addEventListener("keydown", jump);
